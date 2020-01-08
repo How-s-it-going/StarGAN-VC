08b316bafb8f597f0859f2449e1d19b82cf8a2a7
kodamanbou <kodamanbou0424@gmail.com>
2020-01-09 03:04:34 +0900

    Initial commit

---
+++ model.py
@@ -0,0 +1,28 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow_addons as tfa
+
+
+class Generator(tf.keras.Model):
+    def __init__(self):
+        super(Generator, self).__init__()
+        print('tmp')
+
+    def __call__(self, inputs, training=None, mask=None):
+        print('tmp')
+
+
+class Downsample2DBlock(tf.keras.Model):
+    def __init__(self, filters, kernel_size, strides, padding, name_prefix='downsample2d_block_'):
+        super(Downsample2DBlock, self).__init__()
+        self.conv2d = tf.keras.layers.Conv2D(
+            filters, kernel_size=kernel_size, strides=strides, padding=padding, name=name_prefix + 'h1_conv')
+        self.norm = tfa.layers.InstanceNormalization(
+            epsilon=1e-6, name=name_prefix + 'h1_norm')
+        self.conv2d_gates = tf.keras.layers.Conv2D(
+            filters, kernel_size=kernel_size, strides=strides, padding=padding, name=name_prefix + 'h1_gates')
+      sclass  self.norm_gates = tfa.layers.InstanceNormalization(
+            epsilon=1e-6, name=name_prefix + 'h1_norm')
+
+    def call(self, inputs, training=None, mask=None):
+        
