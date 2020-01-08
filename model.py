b4652dde5a8fa9573226154e26cd3b8b10f33024
kodamanbou <kodamanbou0424@gmail.com>
2020-01-09 03:13:34 +0900

    Co-authored-by: 山田 muelu <mueru15@gmail.com>

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 import tensorflow_addons as tfa
+from tf.keras import layers
 
 
 class Generator(tf.keras.Model):
@@ -21,8 +22,16 @@ class Downsample2DBlock(tf.keras.Model):
             epsilon=1e-6, name=name_prefix + 'h1_norm')
         self.conv2d_gates = tf.keras.layers.Conv2D(
             filters, kernel_size=kernel_size, strides=strides, padding=padding, name=name_prefix + 'h1_gates')
-      sclass  self.norm_gates = tfa.layers.InstanceNormalization(
+        self.norm_gates = tfa.layers.InstanceNormalization(
             epsilon=1e-6, name=name_prefix + 'h1_norm')
 
     def call(self, inputs, training=None, mask=None):
-        
+        pass
+
+
+class GatedLinerLayer(tf.keras.Model):
+    def __init__(self):
+        super(GatedLinerLayer, self).__init()
+        self.mul = layers.Multiply()
+
+    def call(self, inputs, training=None, mask=None):
